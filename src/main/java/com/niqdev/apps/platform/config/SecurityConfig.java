095d13ed989c66a1a5f9462880d934ade5f968f8
Nick Chen <nick.chen@cmp.com.tw>
2025-12-30 11:34:41 +0800

    feat: add basic eureka server

---
+++ src/main/java/com/niqdev/apps/platform/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.niqdev.apps.platform.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
